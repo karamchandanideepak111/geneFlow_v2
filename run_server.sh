5477ceb3967701d322cdc546ef02a0055fa6a402
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2024-10-01 10:20:04 +0530

    Create run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Run the Django development server
+python3 manage.py runserver 0.0.0.0:8000 --noreload
