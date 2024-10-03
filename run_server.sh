5cf820a679590636095b5d68f17fc854c46fce9c
karamchandanideepak111 <146127806+karamchandanideepak111@users.noreply.github.com>
2024-10-03 16:15:50 +0530

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -5,4 +5,4 @@ set -e  # Exit on any error
 # source venv/bin/activate
 
 # Run the Django development server
-python3 manage.py runserver 0.0.0.0:8000 --noreload
+python manage.py runserver 0.0.0.0:8000 --noreload
